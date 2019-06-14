146aafd981a37d8441958e8dc2369af5635c3b19
mak20th <mak20th@gmail.com>
2019-06-14 02:23:04 -0500

    created Index.js file

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
+'use strict';
+/* eslint-disable no-undef */
+// main function
+/* global bookmarkList, store, api, Bookmark*/
 
+$(document).ready(function(){
+  bookmarkList.bindEventListeners();
+  bookmarkList.render();
+  api.get_bookmarks()
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(bookmark => store.addBookmark(bookmark));
+      bookmarkList.render();
+    });
+});
